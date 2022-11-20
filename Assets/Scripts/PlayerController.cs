35af85022e70147f84dba74969ddacf34bd93ee9
Neslihan Çetinkaya <cetinkayaneslihan98@gmail.com>
2022-11-20 17:30:02 +0300

    Split screen and local multiplayer added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    [SerializeField] private bool IsFirstPlayer;
     
     void Update()
     {
         // Take Input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
-        
-        // Rotate the vehicle
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (IsFirstPlayer)
+        {
+            // Movement
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed * 1));
+            
+            else if (Input.GetKey(KeyCode.DownArrow))
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed * -1));
+
+            // Rotation
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * -1);
+            
+            else if (Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * 1);
+            
+        }
+        else
+        {
+            // Movement
+            if (Input.GetKey(KeyCode.W))
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed * 1));
+            
+            else if (Input.GetKey(KeyCode.S))
+                transform.Translate(Vector3.forward * (Time.deltaTime * speed * -1));
+
+            // Rotation
+            if (Input.GetKey(KeyCode.A))
+                transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * -1);
+            
+            else if (Input.GetKey(KeyCode.D))
+                transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * 1);
+        }
     }
 }
